71a4c1d68a5d44e97566bd3b79b0c3fa082cb594
le.zhang <le.zhang3@vstecs.com>
2019-11-21 14:41:52 +0800

    ....

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -1,17 +1,15 @@
 import org.apache.log4j.Logger;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
  * Created by Lenovo on 2019/11/4.
  */
 public class Test {
 
-  private static Logger logger=Logger.getLogger(Test.class.getClass());
+  private static Logger logger = Logger.getLogger(Test.class.getClass());
 
-
-
+  @Test
   public void testLog(){
-    for(int i=0;i<10;i++){
+    for(int i = 0;i < 10;i++){
       logger.debug("debug");
       logger.info("info");
     }
